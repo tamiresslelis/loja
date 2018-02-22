e379b5e2d2ac1fdb60c473b61c4bbd315548b5f7
tamiresslelis <tamiresslelis@gmail.com>
2018-02-22 16:04:17 -0300

    inserindo produtos no banco de dados

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -2,6 +2,14 @@
       <?php
         $nome = $_GET["nome"];
         $preco = $_GET["preco"];
-      ?>
-<p class="alert-success">Produto <?php echo $nome; ?>, <?php echo $preco; ?> adicionado com sucesso!</h1>
+        $conexao= mysqli_connect('localhost','root','','loja');
+        $query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+        if(mysqli_query($conexao,$query)){
+          ?><p class="alert-success">Produto <?php echo $nome; ?>, <?php echo $preco; ?> adicionado com sucesso!</h1>
+        <?php }else{ ?>
+          <p class="alert-danger">Produto <?php echo $nome; ?>, não foi adicionado!</h1>
+        <?php
+        }
+        
+        ?>
 <?php include("footer.php")?>
